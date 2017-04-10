f05bf7f7a7046c8d9ad0173c82385db885f2b3d2
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-10 17:17:15 +0100

    first complete attempt at fizzbuzz

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -1,11 +1,19 @@
 describe("Fizzbuzz", function() {
-  // var fizzbuzz
+  var fizzbuzz
 
   beforeEach(function(){
     fizzbuzz = new Fizzbuzz();
   });
 
-  it("returns fizz when number is 3", function() {
+  it("returns fizz when divisible by 3", function() {
     expect(fizzbuzz.play(3)).toEqual('Fizz');
+  });
+
+  it("returns buzz when divisible by 5", function() {
+    expect(fizzbuzz.play(5)).toEqual('Buzz');
+  });
+
+  it("return fizzbuzz when divisible by 3 & 5", function() {
+    expect(fizzbuzz.play(15)).toEqual('Fizzbuzz')
   });
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,7 +1,11 @@
 function Fizzbuzz() {
 }
-  Fizzbuzz.prototype.play = function(number){
-  if (number % 3 === 0) {
-    return 'Fizz';
-  };
-};
+Fizzbuzz.prototype.play = function(number){
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'Fizzbuzz'
+  } else if (number % 3 === 0) {
+    return 'Fizz'
+  } else if (number % 5 === 0) {
+    return 'Buzz'
+  }
+}
